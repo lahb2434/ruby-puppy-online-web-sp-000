8e2310ac99fd0c4c08552e34ded6bb6857c9a877
Chris Houghton <chrishoughton2434@gmail.com>
2020-09-24 16:14:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,4 +9,15 @@ class Dog
     @name = name
     @@all << self
   end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all
+    puts "#{@@all}"
 end
